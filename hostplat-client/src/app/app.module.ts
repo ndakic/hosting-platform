c93938e400ea274693d0d6000e95a4048165860f
Dusan Stevic <stevicdule@gmail.com>
2021-01-03 11:39:13 +0100

    ngx-toastr

--- hostplat-client/src/app/app.module.ts
+++ hostplat-client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,13 @@ import { MaterialModule } from './material/material.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    ToastrModule.forRoot({
+      progressBar: true,
+      timeOut: 4000,
+      closeButton: true,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
