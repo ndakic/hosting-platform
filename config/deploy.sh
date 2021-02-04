ed0304b28a83c1e68d83ce7c725f7fd13dd31157
ndakic <nikoladakic@uns.ac.rs>
2021-02-04 22:55:44 +0100

    deploy update

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-spring_env=prod
+spring_env=dev
 server_ip=18.188.91.54
 user=ubuntu
+pem_file=config/java-api-latest.pem # temp solution
 remote_server=$user@$server_ip
 port=9000
 
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $remote_server "touch test.txt"
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $remote_server "sleep 5s"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $pem_file $remote_server "touch test.txt"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $pem_file $remote_server "sleep 5s"
 
 exit 0
