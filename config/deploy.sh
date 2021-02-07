9be0c3472ef00db9bfcc303dc085b8d9667d1105
ndakic <nikoladakic@uns.ac.rs>
2021-02-07 23:00:13 +0100

    deploy script update

--- config/deploy.sh
+++ config/deploy.sh
@@ -8,6 +8,8 @@ remote_server=$user@$server_ip
 port=9000
 
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $pem_file $remote_server "docker pull ndakic/hostplat-server:latest"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $pem_file $remote_server "docker stop hostplat-server && docker rm hostplat-server"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $pem_file $remote_server "docker run --name hostplat-server -p 9000:9000 ndakic/hostplat-server:latest"
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $pem_file $remote_server "sleep 5s"
 
 exit 0
