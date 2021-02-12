9808b156b75cbddc570d5e35621a97e52681063e
Dusan Stevic <stevicdule@gmail.com>
2021-02-12 01:14:44 +0100

    default avatar feature

--- hostplat-server/src/main/java/uns/ac/rs/hostplatserver/mapper/UserMapper.java
+++ hostplat-server/src/main/java/uns/ac/rs/hostplatserver/mapper/UserMapper.java
@@ -6,10 +6,10 @@ import uns.ac.rs.hostplatserver.model.User;
 
 public class UserMapper {
 	
-	public static UserDTO toDTO(User user) {
-		return new UserDTO(user.getId(), user.getUsername(), user.getFirstName(), user.getLastName(), user.getEmail());
-	}
-	
+    public static UserDTO toDTO(User user) {
+        return new UserDTO(user);
+    }
+		
 	public static User toUser(UserDTO userDto) {
 		return new User(userDto.getId(), userDto.getUsername(), userDto.getFirstName(), userDto.getLastName(), userDto.getEmail());
 	}
